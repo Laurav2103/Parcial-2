cb26fa1d29519634a679a3360bf62cbe0421261b
lauraviddal <lauravidal15@hotmail.com>
2020-12-13 18:50:45 -0500

    definicion de clases

--- Parcial2/cdefensivo.h
+++ Parcial2/cdefensivo.h
@@ -4,10 +4,29 @@
 
 class cDefensivo
 {
+private:
+
+    float d=0;
+    float Xd=d; //pos inicial en x del cañon def
+    float Yd=0; // pos inicial en y
+    float dd=0.025*d; // rango de destruccion
+    float pi=3.1416;
+    float G=9.81;
+
 public:
     cDefensivo();
-    float x;
-    float y;
+    void disparosDefensivos(float Xo,float Yo,int Vini);
+
+    float getD() const;
+    void setD(float value);
+    float getXd() const;
+    void setXd(float value);
+    float getYd() const;
+    void setYd(float value);
+    float getDd() const;
+    void setDd(float value);
+
+
 };
 
 #endif // CDEFENSIVO_H
--- Parcial2/cofensivo.h
+++ Parcial2/cofensivo.h
@@ -4,8 +4,31 @@
 
 class cOfensivo
 {
+private:
+    float d=0;
+    float Xo=0; //pos inicial en x
+    float Yo=0; //pos inicial en y
+    float d0=0.05*d; //rango de destruccion
+
+    float pi=3.1416;
+    float G=9.81;
 public:
     cOfensivo();
+    void disparosOfensivos(float Xd,float Yd,int Vini);
+
+
+    float getD() const;
+    void setD(float value);
+
+    float getXo() const;
+    void setXo(float value);
+
+    float getYo() const;
+    void setYo(float value);
+
+    float getD0() const;
+    void setD0(float value);
+
 };
 
 #endif // COFENSIVO_H
